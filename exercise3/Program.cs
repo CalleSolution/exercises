611cb2cc1b7454f992f9bc195e48fcfee4b9a71a
Carl-Johan Karlsson <carljohan.karlsson@live.se>
2019-09-19 14:56:01 +0200

    exercise 3 solved

---
+++ exercise3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Skriv in ditt förnamn: ");
+            string förnamn = Console.ReadLine();
+            System.Console.Write("Skriv in ditt efternamn: ");
+            string efternamn = Console.ReadLine();
+            
+            Console.WriteLine("Hej " + förnamn + " " + efternamn);
+        }
+    }
+}
