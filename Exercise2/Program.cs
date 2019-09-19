b87eb70dc8766e75e3a492eb9ee7b91276471785
Carl-Johan Karlsson <carljohan.karlsson@live.se>
2019-09-19 14:13:02 +0200

    Solves ex1. Confused named to ex2.

---
+++ Exercise2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Mata in ett tal:");
+            int tal1 = int.Parse(Console.ReadLine());
+            System.Console.WriteLine("Mata in ett tal:");
+            int tal2 = int.Parse(Console.ReadLine());
+
+            int sum = tal1 + tal2;
+            
+            Console.WriteLine("Summan av talet blir: " + sum);
+        }
+    }
+}
