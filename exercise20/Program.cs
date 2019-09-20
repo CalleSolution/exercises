a909996f52d358cf7d7ffb7c06a3b894a62739ec
Carl-Johan Karlsson <carljohan.karlsson@live.se>
2019-09-20 23:26:47 +0200

    exercise20 solved

---
+++ exercise20/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace exercise20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ## Övning 20
+            // Skapa en metod som kontrollerar om man är myndig. Metoden ska ta in ett heltal och returnera true eller false.
+
+                int ageOfUser;
+
+                System.Console.WriteLine("Hur gammal är du?");
+                try{
+                ageOfUser = int.Parse(Console.ReadLine());
+
+                AgeCheck(ageOfUser);
+                }catch{
+                    throw new Exception("Användaren matade in fel tecken..");
+                }
+
+            
+        }
+        static int AgeCheck(int ageOfUser)
+        {
+            bool isMyndig = true;
+
+            if (ageOfUser >= 18){
+                System.Console.WriteLine("Du är: " + isMyndig);
+            }else{
+            isMyndig = false;
+            System.Console.WriteLine("Du är: " + isMyndig);
+            }
+            return ageOfUser;
+        }
+    }
+}
