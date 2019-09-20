2bbc0fe96f5642d18fae03c9a0a0cfcf3e22933b
Carl-Johan Karlsson <carljohan.karlsson@live.se>
2019-09-20 15:53:17 +0200

    Solves solution14

---
+++ Exercise4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hej vad heter du?");
+            string namn = Console.ReadLine();
+            PrintText(namn);
+        }
+        static string PrintText  (string namn)
+        {
+                System.Console.WriteLine("Hello " + namn);
+            return namn;
+        }
+    }
+}
