dfe9fcb705caef27e3eb64d5fc4bfee2281ec3fe
Carl-Johan Karlsson <carljohan.karlsson@live.se>
2019-09-24 14:48:43 +0200

    Solves exercise 10

---
+++ exercise10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace exercise10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //     ## Övning 10
+
+        //     Skriv ett program som räknar baklänges från 40-30 och skriver ut dem på skärmen.
+            
+            for(int i = 40; i >29; i--)
+            {
+                System.Console.WriteLine(i);
+            }
+        }
+    }
+}
