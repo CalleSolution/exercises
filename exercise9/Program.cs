aa06371d4a20ff2e14eceb33c9217f4c1d813f93
Carl-Johan Karlsson <carljohan.karlsson@live.se>
2019-09-24 14:45:06 +0200

    Solves exercise 9

---
+++ exercise9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // # For-loopar
+            // ## Övning 9
+
+            // Skriv ett program som en gång loopar igenom talen 1-10 och skriver ut dem på skärmen.
+
+            // Skapar en for-loop som börjar på noll och loopar fram tills tal < 11, dvs 10(iochmed att 11 inte är mindre än 11).
+                for (int i = 0; i < 11; i++){
+                System.Console.WriteLine(i);
+            }
+        }
+    }
+}
