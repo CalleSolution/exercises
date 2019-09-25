1429477909a91112af482170ff74ea34c2bf7eaf
Carl-Johan Karlsson <carljohan.karlsson@live.se>
2019-09-25 10:33:42 +0200

    Solved exercise 12.1

---
+++ exercise12/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace exercise12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ## Övning 12 
+
+            // Alla namn (del 1)
+            // Skapa en string array med fem element. 
+            // Användaren får mata in fem namn via en for-loop. Programmet skriver sedan ut dessa namn med hjälp av en for-loop. 
+            
+            
+            //skapar en string array med fem index
+            string [] names = new string [5];
+
+            //skapar en forloop där man får mata in namn i de fem indexen
+            //skapar här en try catch också som håller koll att inmatningen är OK.
+            for (int i = 0; i < names.Length; i++)
+            {
+                try{
+                System.Console.Write("Mata in ett namn: ");
+                names[i] = Console.ReadLine();
+                }catch(Exception e)
+                {
+                    System.Console.WriteLine(e);
+                    return;
+                }
+            }
+
+            Console.Clear();
+            //skapar en for-loop där namnen skrivs ut.
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                System.Console.WriteLine("På index " + i + " skrev du in " + names[i]);
+            }
+
+
+        }
+    }
+}
